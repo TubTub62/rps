330269cbac8598e35ff0b4ffb141ff47fc15a82e
Tobias <tobiasalsgaard@gmail.com>
2025-06-08 00:30:32 +0200

    fixed indent and cleaned

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,47 +1,44 @@
 use chrono::Local;
 use tokio::time::{sleep, Duration};
-use tokio::{
-  io::AsyncWriteExt,
-  net::{TcpListener, TcpStream},
-};
-
+use tokio::net::{TcpListener, TcpStream};
+use tokio::io::AsyncWriteExt;
 
 async fn process(mut socket: TcpStream) {
-  println!("Connected this client, Remote Addr: {:?}", socket.peer_addr().unwrap());
-  socket.set_nodelay(true).unwrap();
+	println!("Connected to client - Remote Addr: {:?}", socket.peer_addr().unwrap());
+	socket.set_nodelay(true).unwrap();
 
-  loop {
-    sleep(Duration::from_secs(1)).await;
+	loop {
+		sleep(Duration::from_secs(1)).await;
 
-    let local = Local::now();
-    let time = local.format("%H:%M:%S\n").to_string();
+		let local = Local::now();
+		let time = local.format("%H:%M:%S\n").to_string();
 
-    if let Err(e) = socket.write_all(time.as_bytes()).await {
-      println!("During send, socket has given error: {}", e);
-      return;
-    }
-  }
+		if let Err(e) = socket.write_all(time.as_bytes()).await {
+			println!("During send, socket has given error: {}", e);
+			return;
+		}
+	}
 }
 
 
 #[tokio::main]
 async fn main() {
 
-    let port = String::from("4000");
-    let listener = TcpListener::bind(format!("0.0.0.0:{}", port)).await.unwrap();
+	let ip = String::from("127.0.0.1");
+	let port = String::from("4000");
+	let listener = TcpListener::bind(format!("{}:{}", ip, port)).await.unwrap();
 
-    println!("Application started");
-    
-    loop {
-      println!("Listening");
-        match listener.accept().await {
-        Ok((socket, _)) => {
-            tokio::spawn(async move {
-            process(socket).await;
-            });
-        }
-        Err(e) => println!("Error in acception: {}", e),
-        }
-    }
+	println!("Server Started");
+
+	loop {
+		match listener.accept().await {
+			Ok((stream, _)) => {
+				tokio::spawn(async move {
+				process(stream).await;
+				});
+			}
+			Err(e) => println!("Error in acception: {}", e),
+		}
+	}
 
 }
